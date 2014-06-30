2619dbf63fe66db9303097bd5ba3369768cbdd93
0x7674 <veetii@gmail.com>
2014-06-30 03:18:46 -0700

    Fewer stupid bugs; Smartened 404 handling.

--- multipass_threaded.py
+++ multipass_threaded.py
@@ -2,17 +2,17 @@
 
 # TODO
 # urllib2 timeout
-# smart 'fake 404' handling
 
 import urllib2
 import sys
+import signal
 import socket
 import re
 from threading import Thread
 from Queue import Queue, Empty
 
 queue = Queue()
-headers = {'X-Forwarded-For': '127.0.0.1'}
+headers = {'X-Forwarded-For': '127.0.0.1', 'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
 threadcount = 10
 results = {}
 ports = [80, 443]
@@ -25,6 +25,12 @@ bf_wordlist = bf_wordlist_f.readlines()
 
 domain_list_f.close()
 bf_wordlist_f.close()
+
+def signal_handler(signal, frame):
+	print "\nCtrl-C pressed, exiting.."
+	sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 def test_domain(domain):
 	# check domain resolves
@@ -50,69 +56,59 @@ def get_404_control(scheme, domain, port):
 	print "Checking 404 for " + scheme + domain + ':' + `port`
 	control = {}
 	try:
-		response_404_1 = urllib2.urlopen(scheme + domain + ':' + `port` + "/" + "Neg-Existorz")
-		response_404_2 = urllib2.urlopen(scheme + domain + ':' + `port` + "/" + "Neg-ExistorzNeg-Existorz")
+		req1 = urllib2.Request(scheme + domain + ':' + `port` + "/" + "Neg-Existorz", None, headers)
+		response_404_1 = urllib2.urlopen(req1)
+		req2 = urllib2.Request(scheme + domain + ':' + `port` + "/" + "Neg-ExistorzNeg-Existorz", None, headers)
+		response_404_2 = urllib2.urlopen(req2)
 		len_404_1 = len(response_404_1.read())
 		len_404_2 = len(response_404_2.read())
 		if (len_404_1 != len_404_2) and ( ( (len_404_2 % 12) != 0) and ( (len_404_1 % 12) != 0) ):
-			print "Something funky is going on here. Review manually - Skipping.."
-			control['type'] = 'wtf'
+			print "weird length responses"
+			control['type404'] = 'wtf'
 			control['staticlen'] = 'wtf'
 			control['multiplier'] = 'wtf'
 		else:
 			if (len_404_2-len_404_1) == 0:
-				print "A 404 response is a static size of " + `len_404_1`
-				control['type'] = 'static200'
+				control['type404'] = 'static200'
 				control['staticlen'] = len_404_1
 				control['multiplier'] = 1
 			else:
 				difference = (len_404_2-len_404_1)/12
-				print "A 404 response is " + `difference` + " * request length + " + `(len_404_1-(difference * 12))`
-				control['type'] = 'variable200'
+				control['type404'] = 'variable200'
 				control['staticlen'] = (len_404_1-(difference * 12))
 				control['multiplier'] = difference
-			#logfile.write(scheme + domain + ':' + `port` + ' forges 404 responses. Skipping..\n')
 	except urllib2.HTTPError as errorDetail:
 		cleaned = errorClean(errorDetail)
 		if cleaned == 404:
-			control['type'] = 'standard'
-	except urllib2.URLError as urlError:
-		control['type'] = 'not-https'
+			control['type404'] = 'standard'
+			control['staticlen'] = 0
+			control['multiplier'] = 1
+		else:
+			print "some httperror"
+			control['type404'] = 'wtf'
 
-	print "control:"
-	print control
+	except urllib2.URLError as urlError:
+		print "some urlerror"
+		control['type404'] = 'wtf'
+	except:
+		print "some timeout, probably. catchall"
+		# probably a timeout of some description
+		control['type404'] = 'wtf'
+
 	return control
 
-"""
-def check_page(scheme, domain, port, page, ex404):
-	res_code = ''
-	try:
-		req = urllib2.Request(scheme+domain+':'+`port`+'/'+page, None, headers)
-		#check_response = urllib2.urlopen(scheme + domain + ':' + `port` + '/' + page)
-		get_response = urllib2.urlopen(req)
-		#res_code = get_response.getcode()
-		#return res_code, len(get_response.read())
-		return parse_response(get_response, len(page), ex404)
-	except urllib2.HTTPError as errorDetail:
-		# 40x error
-		return errorClean(errorDetail), 0
-	except urllib2.URLError as urlError:
-		return 'Timeout', 0
-"""
-
-def cp_thread(my404={}):
-	print "my404:"
-	print my404
+def cp_thread(**kwargs):
 	try:
 		while True:
 			url = queue.get_nowait()
 			try:
 				req = urllib2.Request(url, None, headers)
 				http_response = urllib2.urlopen(url)
-				response_parsed = parse_response(http_response, url, my404)  #XXX hmmmmmm
+				response_parsed = parse_response(http_response, url, **kwargs) 
 				res_code = response_parsed['code']
 				res_length = response_parsed['len']
-				sys.stdout.write("!"),
+				res_mark = response_parsed['mark']
+				sys.stdout.write(res_mark),
 				sys.stdout.flush()
 			except urllib2.HTTPError as errorDetail:
 				sys.stdout.write("."),
@@ -122,6 +118,11 @@ def cp_thread(my404={}):
 			except urllib2.URLError as urlError:
 				sys.stdout.write("x"),
 				res_code = 1 # timeout
+				res_length = 0
+				sys.stdout.flush()
+			except:
+				sys.stdout.write("X"),
+				res_code = 1
 				res_length = 0
 				sys.stdout.flush()
 
@@ -141,33 +142,38 @@ def errorClean(errorRaw):
 		return 403
 	elif errorMsg == "HTTP Error 401: Unauthorized":
 		return 401
+	else:
+		return errorRaw
 
-def parse_response(response_object, url, handler404={}):
-	print "handler404:"
-	print handler404
+def parse_response(response_object, url, type404, staticlen, multiplier):
 	request_len = re.sub('http.+:\d+\/','',url)
 	parsed_results = {}
 	response_len = len(response_object.read())
-	if handler404['type'] == 'standard':
+	if type404 == 'standard':
 		parsed_results['code'] = 200
 		parsed_results['len'] = response_len
+		parsed_results['mark'] = '!'
 		# If it was a 400 error it wouldn't have made it this far anyway		
 	
-	elif handler404['type'] == 'static200':
-		if handler404['staticlen'] == response_len:
+	elif type404 == 'static200':
+		if staticlen == response_len:
 			parsed_results['code'] = 404
 			parsed_results['len'] = 0
+			parsed_results['mark'] = '.'
 		else:
 			parsed_results['code'] = 200
 			parsed_results['len'] = response_len
+			parsed_results['mark'] = '!'
 
-	elif handler404['type'] == 'variable200':
-		if len(response_object.read()) == (handler404['staticlen'] + (handler404['multiplier'] * request_len)):
+	elif type404 == 'variable200':
+		if len(response_object.read()) == (staticlen + (multiplier * request_len)):
 			parsed_results['code'] = 404
 			parsed_results['len'] = 0
+			parsed_results['mark'] = '.'
 		else:
 			parsed_results['code'] = 200
 			parsed_results['len'] = response_len
+			parsed_results['mark'] = '!'
 
 	return parsed_results
 
@@ -199,20 +205,18 @@ for domain in domain_list:
 
 		control_404 = {}
 		control_404 = get_404_control(scheme, dom, port)
-		if control_404['type'] == 'not-https':
+		if control_404['type404'] == 'not-https':
 			print "Port " + `port` + ' is not running HTTPS as expected. Skipping..'
 			continue
-		elif control_404['type'] == 'wtf':
+		elif control_404['type404'] == 'wtf':
 			print "Port " + `port` + ' needs to be assessed manually, skipping..'
 			continue
-		elif control_404['type'] == 'static200':
+		elif control_404['type404'] == 'static200':
 			print "Static 404 response size of " + `control_404['staticlen']` + "."
-		elif control_404['type'] == 'variable200':
+		elif control_404['type404'] == 'variable200':
 			print "Variable 404 response size. "
-			# TODO set up the control 404 handlre
-		elif control_404['type'] == 'standard':
+		elif control_404['type404'] == 'standard':
 			print "normal 404."
-
 		print "Bruting " + scheme + dom + ':' + `port` + '..'
 
 		for testcase in bf_wordlist:
@@ -220,7 +224,7 @@ for domain in domain_list:
 			queue.put(scheme + dom + ':' + `port` + '/' + page)
 		workers = []
 		for i in range(threadcount):
-			worker = Thread(group=None, target=cp_thread, name=None, args=(control_404), kwargs={})
+			worker = Thread(group=None, target=cp_thread, name=None, args=(), kwargs=control_404)
 			worker.start()
 			workers.append(worker)
 		for worker in workers:
@@ -234,8 +238,8 @@ for domain in domain_list:
 
 			logfile.write(res + ' : ' + rcode + ' (' + `results[res]['length']` + ' bytes)\n')
 
+		results = {}
 		sys.stdout.write("\n")
 		sys.stdout.flush()		
 
 logfile.close()
-
